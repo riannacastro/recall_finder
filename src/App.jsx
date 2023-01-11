dfa321501b77f0727f80aede95b793dd7ba8f823
Rianna Castro <riannaxcastro@gmail.com>
2023-01-10 16:24:54 -1000

    set up react router

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Home from './components/Home';
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route
+ } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
