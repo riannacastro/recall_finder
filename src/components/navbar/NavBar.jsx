b6d0c3fa797dc30c5c7345d2d49022e4894b97ae
Rianna Castro <riannaxcastro@gmail.com>
2023-01-13 14:39:15 -1000

    change h3 to buttons on navbar and added onclick

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './../css/NavBar.css'
 import { BsSearch } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
 
 export default function NavBar() {
+  const navigate = useNavigate();
+
   return (
     <div className='header'>
-      <h1>Recall <BsSearch /> Finder</h1>
-      <h3>Home</h3>
-      <h3>Food Recalls</h3>
-      <h3>Drug Recalls</h3>
-      <h3>Device Recalls</h3>
+      <h1><button>Recall <BsSearch /> Finder</button></h1>
+      <button>Home</button>
+      <button onClick={() => navigate('/food-recalls')}>Food Recalls</button>
+      <button onClick={() => navigate('/drug-recalls')}>Drug Recalls</button>
+      <button onClick={() => navigate('/device-recalls')}>Device Recalls</button>
       <form>
         <p>Looking for something specific?</p>
         <label>Search for product: </label>
