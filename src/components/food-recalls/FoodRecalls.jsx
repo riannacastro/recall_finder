d77f5087c1c4790b6973ed7dcb267b6ccea902ab
Rianna Castro <riannaxcastro@gmail.com>
2023-01-19 15:09:03 -1000

    set up food recalls page

--- src/components/food-recalls/FoodRecalls.jsx
+++ src/components/food-recalls/FoodRecalls.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import NavBar from '../navbar/NavBar';
+import '../css/FoodRecalls.css';
 
 export default function FoodRecalls() {
+  const [data, setData] = useState();
+
+  const getData = () => {
+    axios.get('https://api.fda.gov/food/enforcement.json?search=report_date:[20220101+TO+20221231]&limit=10')
+    .then((resp) => {
+      setData(resp.data.results)
+      console.log(data)
+    });
+  };
+  
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const tableData = () => {
+      return data.map((val) => {
+        return <tr key={val.recall_number}>
+          <td>{val.product_type}</td>
+          <td>{val.report_date}</td>
+          <td>{val.product_description}</td>
+          <td>{val.reason_for_recall}</td>
+        </tr>
+      })
+  }
+
+
   return (
-    <div>FoodRecalls</div>
+    <div>
+      <NavBar />
+      <div className='title'>
+        <h1>Food Recalls</h1>
+      </div>
+      { data ? 
+        <table className='center'>
+          <tr>
+            <th>Category </th>
+            <th>Recall Date</th>
+            <th>Product Name </th>
+            <th>Reason </th>
+          </tr>
+          <tbody>
+            {tableData()}
+          </tbody>
+    </table> 
+    : <></>}
+    </div>
   )
 }
